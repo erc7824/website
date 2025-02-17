5fcc41812b51dacd7ef9439688fd3f495a9dfa14
akushniruk <akushniruk@openware.com>
2025-02-17 11:46:08 +0100

    fix: change project social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/erc7824_social_card.png',
       navbar: {
         logo: {
           alt: 'ERC-7824',
